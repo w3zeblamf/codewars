d1a3485dd5c6991ed46442dcf906dc3b78cff758
w3zeblamf <zeblamfw3@gmail.com>
2022-05-22 18:24:20 -0400

    files updated

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# codewars
+# My codewars kata solutions
+
+## Language
+
+- JavaScript
+
+## Ranks
+
+### 7 kyu
+
+Solved Kata list: 
+
+### 8 kyu
+
+Solved Kata list: 
+
+- 01. Convert a Number to a String! ==> https://www.codewars.com/kata/5265326f5fda8eb1160004c8
---
+++ Rank_8-kyu/01.- convert-a-number-to-a-string/script.js
@@ -0,0 +1,49 @@
+/* Remember to use 'strict' mode in all scripts now!
+You can use strict mode in all your programs. It helps you to write cleaner code,
+like preventing you from using undeclared variables. (https://www.w3schools.com/js/js_strict.asp) */
+'use strict';
+
+//*Kata training 1: Convert a Number to a String!
+//*==============================================
+
+/* Details:
+
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+123 --> "123"
+999 --> "999" */
+
+//Solution 1: (My Solution)
+
+function numberToString(num) {
+  return num.toString() // 123.toString() = '123'
+}
+
+console.log(numberToString(123)); // '123'
+console.log(typeof(numberToString(123))); // '123'
+
+//Other solutions
+
+//Solution 2:
+
+// function numberToString2(num) {
+//   return `${num}`;
+// }
+
+// console.log(numberToString2(520)); // '520'
+
+//Solution 3:
+
+// function numberToString3(num) {
+
+//   let str = '';
+
+//   str += num; // str = srt + num = ' ' + num
+
+//   return str;
+// }
+
+// console.log(numberToString3(625)); // '625
