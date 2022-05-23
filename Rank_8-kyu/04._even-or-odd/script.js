7c8506d104e77378790860b8507284891bb0a965
w3zeblamf <zeblamfw3@gmail.com>
2022-05-23 16:35:24 -0400

    even-or-odd solution

---
+++ Rank_8-kyu/04._even-or-odd/script.js
@@ -0,0 +1,37 @@
+/* Remember to use 'strict' mode in all scripts now! 
+You can use strict mode in all your programs. It helps you to write cleaner code, 
+like preventing you from using undeclared variables. (https://www.w3schools.com/js/js_strict.asp) */
+'use strict';
+
+/* 
+Details: 
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
+
+//Solution 1 (My Solution):
+function even_or_odd(number) {
+  if (number % 2) {
+    return `${number} is an 'Odd' number`;
+  } else {
+    return `${number} is an Even number`
+  }
+}
+
+console.log(even_or_odd(2));
+console.log(even_or_odd(-9));
+console.log(even_or_odd(-8));
+console.log(even_or_odd(5));
+
+//Other Solutions
+
+//Solution 2:
+
+// const even_or_odd = (number) => (number % 2) ? 'Odd': 'Even';
+
+// console.log(even_or_odd(2));
+// console.log(even_or_odd(-9));
+// console.log(even_or_odd(-8));
+// console.log(even_or_odd(5));
+
+
+//MDN Web Docs reference:
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder
