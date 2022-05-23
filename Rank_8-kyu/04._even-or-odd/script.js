7953af69820f2cdbbc675ef998c75f15e3a2811d
w3zeblamf <zeblamfw3@gmail.com>
2022-05-23 16:46:53 -0400

    even-or-odd solution

--- Rank_8-kyu/04._even-or-odd/script.js
+++ Rank_8-kyu/04._even-or-odd/script.js
@@ -3,11 +3,16 @@ You can use strict mode in all your programs. It helps you to write cleaner code
 like preventing you from using undeclared variables. (https://www.w3schools.com/js/js_strict.asp) */
 'use strict';
 
+//*Kata training 4: Even or Odd
+//*=============================
+
 /* 
 Details: 
 Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
 
 //Solution 1 (My Solution):
+//=========================
+
 function even_or_odd(number) {
   if (number % 2) {
     return `${number} is an 'Odd' number`;
@@ -22,6 +27,7 @@ console.log(even_or_odd(-8));
 console.log(even_or_odd(5));
 
 //Other Solutions
+//===============
 
 //Solution 2:
 
@@ -34,4 +40,5 @@ console.log(even_or_odd(5));
 
 
 //MDN Web Docs reference:
+//=======================
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder
