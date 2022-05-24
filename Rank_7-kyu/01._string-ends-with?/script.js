5c4b776a1d6d2c1df144504d01a94467c2e2c5e1
w3zeblamf <zeblamfw3@gmail.com>
2022-05-24 19:41:36 -0400

    string-ends-with? solution

---
+++ Rank_7-kyu/01._string-ends-with?/script.js
@@ -0,0 +1,37 @@
+/* Remember to use 'strict' mode in all scripts now! 
+You can use strict mode in all your programs. It helps you to write cleaner code, 
+like preventing you from using undeclared variables. (https://www.w3schools.com/js/js_strict.asp) */
+'use strict';
+
+//**Kata training 01: String ends with?
+//*===============================================
+
+/* Details: 
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+//Solution 1 (My Solution):
+//=========================
+
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
+
+console.log(solution('abc', 'bc')); // returns true
+console.log(solution('abc', 'd')); // returns false
+
+
+//Other Solutions
+//===============
+
+
+
+
+//MDN Web Docs reference:
+//=======================
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
