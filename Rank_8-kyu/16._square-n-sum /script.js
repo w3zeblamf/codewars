ba8c1b95f387ef2f0e10433f02f0a6e8c8176aa0
w3zeblamf <zeblamfw3@gmail.com>
2022-05-31 16:01:38 -0400

    square-n-sum solution

--- Rank_8-kyu/16._square-n-sum /script.js
+++ Rank_8-kyu/16._square-n-sum /script.js
@@ -3,7 +3,7 @@ You can use strict mode in all your programs. It helps you to write cleaner code
 like preventing you from using undeclared variables. (https://www.w3schools.com/js/js_strict.asp) */
 'use strict';
 
-//**Kata training 16:
+//**Kata training 16: Square(n) Sum
 //*====================================================
 
 /* Details: 
