eb9fb546701e480604d6c3f9bbfae3c0b7e46f60
w3zeblamf <zeblamfw3@gmail.com>
2022-06-07 10:44:43 -0400

    dna-to-rna-conversion solution

---
+++ Rank_8-kyu/23._dna-to-rna-conversion/script.js
@@ -0,0 +1,39 @@
+/* Remember to use 'strict' mode in all scripts now!
+You can use strict mode in all your programs. It helps you to write cleaner code,
+like preventing you from using undeclared variables. (https://www.w3schools.com/js/js_strict.asp) */
+'use strict';
+
+//*Kata training 21: Sum without highest and lowest number
+//*=======================================================
+
+/* Details:
+
+*/
+
+//Solution 1: (My Solution)
+//=========================
+
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, 'U');
+}
+
+console.log(DNAtoRNA('GCAT')); //'GCAU'
+
+
+//Other solutions
+
+//Solution 2:
+//============
+
+function DNAtoRNA(dna) {
+  return dna.split("T").join("U");
+}
+
+console.log(DNAtoRNA('GCAT')); //'GCAU'
+
+
+//MDN/w3schools Web Docs reference:
+//=================================
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+//https://www.w3schools.com/jsref/jsref_replace.asp
